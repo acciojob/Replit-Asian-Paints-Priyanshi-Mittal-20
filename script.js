df065c63642fba48d1fd4579d5971d5ea9fcc9a0
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-09-18 10:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let container= document.getElementById("grid-container")
+let colorBtn= document.getElementById("change_button")
+let resetBtn= document.getElementById("Reset")
+let inputColor= document.getElementById("colour_id")
+let inputBlock= document.getElementById("block_id")
+colorBtn.addEventListener("click",()=>{
+	let colour=inputColor.value
+	let block= inputBlock.value
+	let change=document.getElementById(`${block}`)
+	change.style.backgroundColor=`${colour}`
+})
+resetBtn.addEventListener("click",()=>{
+	let colour=inputColor.value
+	let block= inputBlock.value
+	let change=document.getElementById(`${block}`)
+	change.style.backgroundColor="white";
+})
